4c0536e7f0dc20ed958431f3922bb70f0013d3ba
yegorf1 <yegorf1@gmail.com>
2018-12-30 16:54:13 +0300

    Add posts to main page

--- YogurtTheBlog/ClientApp/src/components/Home.js
+++ YogurtTheBlog/ClientApp/src/components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
+import {bindActionCreators} from "redux";
+import {actionCreators} from "../store/Posts";
+import Post from "./Post";
 
 class Home extends React.Component {
+    componentDidMount() {
+        this.props.requestPosts(this.props.page);
+    }
+    
     render() {
-        return <h1>WIP</h1>;
+        return <div id="posts" className="content" style={{width: "100%"}}>
+            {this.props.posts.map(post => <Post key={post.constantUrl} post={post}/>)}
+        </div>;
     }
 }
-export default connect()(Home);
+
+export default connect(
+    state => state.posts,
+    dispatch => bindActionCreators(actionCreators, dispatch)
+)(Home);
