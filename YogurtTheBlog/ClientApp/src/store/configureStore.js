a59928363dc3afbd9d4f9d5db075e8c525f3e24a
yegorf1 <yegorf1@gmail.com>
2018-12-30 16:52:16 +0300

    Register posts store

--- YogurtTheBlog/ClientApp/src/store/configureStore.js
+++ YogurtTheBlog/ClientApp/src/store/configureStore.js
@@ -2,12 +2,11 @@ import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import * as Counter from './Counter';
-import * as WeatherForecasts from './WeatherForecasts';
-
+import * as Posts from "./Posts";
 export default function configureStore (history, initialState) {
   const reducers = {
     counter: Counter.reducer,
-    weatherForecasts: WeatherForecasts.reducer
+    posts: Posts.reducer
   };
 
   const middleware = [
