ecfacd4e85d83d3351486c32792d07a2d59867f8
yegorf1 <yegorf1@gmail.com>
2018-12-30 13:44:13 +0300

    Change NavMenu to Header in Layout

--- YogurtTheBlog/ClientApp/src/components/Layout.js
+++ YogurtTheBlog/ClientApp/src/components/Layout.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Container } from 'reactstrap';
-import NavMenu from './NavMenu';
+import Header from './Header';
 
 export default props => (
   <div>
-    <NavMenu />
+    <Header />
     <Container>
       {props.children}
     </Container>
