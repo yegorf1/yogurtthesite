13e67da458cddeb4c19a1e3fa691d64894848ef8
yegorf1 <yegorf1@gmail.com>
2018-12-31 00:16:09 +0300

    Add user model

---
+++ YogurtTheBlog/Models/User.cs
@@ -0,0 +1,7 @@
+namespace YogurtTheBlog.Models {
+    public class User {
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
