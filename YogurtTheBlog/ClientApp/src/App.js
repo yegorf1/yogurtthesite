d4e906a3dee709a2da19b337a7069a04f1bbfcfd
yegorf1 <yegorf1@gmail.com>
2018-12-31 00:19:09 +0300

    Add login and register pages to router

--- YogurtTheBlog/ClientApp/src/App.js
+++ YogurtTheBlog/ClientApp/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import React from 'react';
-import { Route } from 'react-router';
+import {Route} from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Post from "./components/Post";
 import Resume from "./components/Resume";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
 
 export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/p/:postUrl' component={Post} />
-    <Route exact path='/resume' component={Resume} />
-  </Layout>
+    <Layout>
+        <Route exact path='/' component={Home}/>
+        <Route path='/p/:postUrl' component={Post}/>
+        <Route exact path='/resume' component={Resume}/>
+        <Route exact path='/login' component={LoginPage}/>
+        <Route exact path='/register' component={RegisterPage}/>
+    </Layout>
 );
