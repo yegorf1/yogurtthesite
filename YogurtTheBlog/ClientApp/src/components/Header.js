7d649376d5bbf79ee1d2817626c0b77a4ef87796
yegorf1 <yegorf1@gmail.com>
2018-12-31 15:17:41 +0300

    Reworked links in Header

--- YogurtTheBlog/ClientApp/src/components/Header.js
+++ YogurtTheBlog/ClientApp/src/components/Header.js
@@ -2,21 +2,59 @@ import '../stylesheets/Header.css';
 import * as React from "react";
 import {Link} from "react-router-dom";
 import connect from "react-redux/es/connect/connect";
+import {bindActionCreators} from "redux";
+import {actionCreators} from "../store/Auth";
 
 class Header extends React.Component {
+    getLinks() {
+        let links = [
+            {
+                link: '/',
+                text: 'Блог'
+            }, {
+                link: '/resume',
+                text: 'Резюме'
+            }
+        ];
+
+        if (this.props.loggedIn) {
+            links.push({
+                link: '/',
+                text: 'Выход',
+                onClick: this.props.logout
+            });
+
+            if (this.props.user.isAdmin) {
+                links.push({
+                    link: '/new',
+                    text: 'Пост'
+                });
+            }
+        }
+
+        return links;
+    }
+
     render() {
         return <div id="header">
             <div className="logo">
                 <h1>
-                    Yogurt The{' '}
-                    <Link to="/">Блог</Link>
-                    /
-                    <Link to="/resume">Резюме</Link>
+                    Yogurt The Horse
+                    {
+                        this.getLinks().map(link => (
+                            <div>
+                                {'/'}
+                                <Link to={link.link} onClick={link.onClick}>
+                                    {link.text}
+                                </Link>
+                            </div>
+                        ))
+                    }
                 </h1>
             </div>
             {
                 this.props.loggedIn &&
-                <div className="float-right">Logged as {this.props.user.username}</div>
+                <div className="float-right">Logged in as {this.props.user.username}</div>
             }
             <div className="separator"/>
         </div>;
@@ -24,5 +62,6 @@ class Header extends React.Component {
 }
 
 export default connect(
-    state => state.auth
+    state => state.auth,
+    dispatch => bindActionCreators(actionCreators, dispatch)
 )(Header);
