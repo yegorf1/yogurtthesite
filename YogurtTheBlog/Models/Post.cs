ae1ab063c206a034448fd5e0196a79cb8d42b18b
yegorf1 <yegorf1@gmail.com>
2018-12-31 15:49:32 +0300

    Add author field to post

--- YogurtTheBlog/Models/Post.cs
+++ YogurtTheBlog/Models/Post.cs
@@ -6,5 +6,6 @@ namespace YogurtTheBlog.Models {
         public DateTime PublishDate { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
+        public string Author { get; set; }
     }
 }
