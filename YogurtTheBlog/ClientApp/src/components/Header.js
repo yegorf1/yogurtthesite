3151fcd1a90b2a87d362b63bdd093e13d9fa35fc
yegorf1 <yegorf1@gmail.com>
2018-12-31 14:52:38 +0300

    Add info about user to header

--- YogurtTheBlog/ClientApp/src/components/Header.js
+++ YogurtTheBlog/ClientApp/src/components/Header.js
@@ -1,19 +1,28 @@
 import '../stylesheets/Header.css';
 import * as React from "react";
 import {Link} from "react-router-dom";
+import connect from "react-redux/es/connect/connect";
 
-export default class Header extends React.Component {
+class Header extends React.Component {
     render() {
         return <div id="header">
             <div className="logo">
                 <h1>
-                    Yogurt The{' '} 
+                    Yogurt The{' '}
                     <Link to="/">Блог</Link>
                     /
                     <Link to="/resume">Резюме</Link>
                 </h1>
             </div>
+            {
+                this.props.loggedIn &&
+                <div className="float-right">Logged as {this.props.user.username}</div>
+            }
             <div className="separator"/>
         </div>;
     }
 }
+
+export default connect(
+    state => state.auth
+)(Header);
