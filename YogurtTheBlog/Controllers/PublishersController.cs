4fad730a43730f9680eeaefdc072b855ef3ee05f
yegorf1 <yegorf1@gmail.com>
2019-01-04 22:00:44 +0300

    Add publishers controllers

---
+++ YogurtTheBlog/Controllers/PublishersController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using YogurtTheBlog.Models;
+using YogurtTheBlog.Repositories;
+
+namespace YogurtTheBlog.Controllers {
+    [Authorize(Roles = "Admin")]
+    [Route("api/{controller}")]
+    public class PublishersController : Controller {
+        private readonly IPublishersRepository _publishers;
+
+        public PublishersController(IPublishersRepository publishers) {
+            _publishers = publishers;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Publisher>> GetPublishers() {
+            return await _publishers.GetPublishers();
+        }
+
+        [HttpPut]
+        public async Task CreatePublisher(Publisher publisher) {
+            await _publishers.AddPublisher(publisher);
+        }
+
+        [HttpDelete("{name}")]
+        public async Task DeletePublisher(string name) {
+            await _publishers.DeletePublisher(name);
+        }
+    }
+}
